2716e89cdcce82f57f8414dd754dd4f1fb07da69
grant <grantphillips125@gmail.com>
2026-04-19 23:08:02 -0400

    no sep

--- main/main.cpp
+++ main/main.cpp
@@ -2,6 +2,8 @@
 #include <memory>
 #include <string>
 #include <typeinfo>
+#include <fstream>
+#include <sstream>
 #include <cadmium/modeling/celldevs/asymm/coupled.hpp>
 #include <cadmium/modeling/celldevs/asymm/config.hpp>
 #include <cadmium/simulation/logger/csv.hpp>
@@ -19,6 +21,25 @@ std::shared_ptr<AsymmCell<flood_state, double>> addFloodCell(
 		return std::make_shared<flood_cell>(cellId, cellConfig);
 	}
 	throw std::bad_typeid();
+}
+
+void removeSepLineIfPresent(const std::string& filename) {
+	std::ifstream in(filename);
+	if (!in.is_open()) return;
+
+	std::string firstLine;
+	std::getline(in, firstLine);
+
+	if (firstLine != "sep=;") {
+		return;
+	}
+
+	std::ostringstream remaining;
+	remaining << in.rdbuf();
+	in.close();
+
+	std::ofstream out(filename, std::ios::trunc);
+	out << remaining.str();
 }
 
 int main(int argc, char** argv) {
@@ -42,6 +63,7 @@ int main(int argc, char** argv) {
 	rootCoordinator.start();
 	rootCoordinator.simulate(simTime);
 	rootCoordinator.stop();
+	removeSepLineIfPresent("flood_log.csv");
 
 	return 0;
 }
